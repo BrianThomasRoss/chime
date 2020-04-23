74f0357ebb5bd6c4ccc3864db3d539bb7530a2c3
BrianThomasRoss <ml@brianthomasross.com>
2020-04-23 15:14:14 -0400

    Rewrite setup for PyPI

--- setup.py
+++ setup.py
@@ -1,3 +1,71 @@
+"""setup for PyPI deploy
+"""
+
+from setuptools import find_packages, setup
+
+VERSION = "1.1.3"  # update VERSION in constants.py
+AUTHOR = "Predictive Healthcare @ Penn Medicine"
+MAINTAINER = "Brian Ross, Code for Philly"
+MAINTAINER_EMAIL = "ml@brianthomasross.com"
+URL = "https://codeforphilly.github.io/chime/"
+PROJECT_URLS={
+    "Bug Reports": "https://github.com/CodeForPhilly/chime/issues",
+    "Source": "https://github.com/CodeForPhilly/chime",
+    "Documentation": "https://codeforphilly.github.io/chime/",
+}
+REQUIREMENTS = [
+            "altair",
+            "gspread",
+            "numpy",
+            "pandas",
+            "pytest",
+            "streamlit",
+            "gspread",
+            "oauth2client"
+]
+with open("README.rst") as rm:
+    README = rm.read()
+CLASSIFIERS = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
+
+setup(
+    name="penn-chime",
+    version=VERSION,
+    author=AUTHOR,
+    author_email="",
+    description="Covid-19 Hospital Impact Model for Epidemics",
+    long_description=README,
+    url=URL,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    test_suite='tests',
+
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': ['penn_chime=penn_chime.cli:main'],
+    },
+    include_package_data=True,
+)
+
+
+
+
+
+
+
+
+
+
+
+
 """Setup file for chime
 """
 __version__ = "1.1.3"  # update VERSION in constants.py
@@ -6,44 +74,4 @@ __author__ = "Predictive Healthcare @ Penn Medicine"
 from setuptools import setup, find_namespace_packages
 
 
-setup(
-    name="penn_chime",
-    version=__version__,
-    author=__author__,
-    author_email="",
-    description="COVID-19 Hospital Impact Model for Epidemics",
-    url="https://github.com/CodeForPhilly/chime",
-    project_urls={
-        "Bug Reports": "https://github.com/CodeForPhilly/chime/issues",
-        "Source": "https://github.com/CodeForPhilly/chime",
-        "Documentation": "https://codeforphilly.github.io/chime/",
-    },
-    package_dir={'': 'src'},
-    packages=find_namespace_packages(where='src', exclude=('tests')),
-    install_requires=[
-        "altair",
-        "black",
-        "gspread",
-        "gunicorn",
-        "dash",
-        "dash_bootstrap_components",
-        "numpy",
-        "pandas",
-        "pytest",
-        "pyyaml",
-        "streamlit",
-        "gspread",
-        "oauth2client"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-    entry_points={
-        'console_scripts': ['penn_chime=penn_chime.cli:main'],
-    },
-    keywords=[],
-    include_package_data=True,
-)
+
